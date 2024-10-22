01db4b74eabf671dd326f788b0eb46453327b5fa
Горбунов Андрій <andreigorbunov044@gmail.com>
2024-10-23 05:10:13 +0800

    Добавьте файлы проекта.

---
+++ lab7/Controllers/FileController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace lab7.Controllers
+{
+    public class FileController : Controller
+    {
+        [HttpGet]
+        public IActionResult DownloadFile()
+        {
+            return View("~/Views/Home/DownloadFile.cshtml");
+        }
+
+        [HttpPost]
+        public IActionResult DownloadFile(string name, string surname, string fileName)
+        {
+            string content = $"Name: {name}\nSurname: {surname}";
+            byte[] fileBytes = Encoding.UTF8.GetBytes(content);
+            string fileDownloadName = $"{fileName}.txt";
+
+            return File(fileBytes, "text/plain", fileDownloadName);
+        }
+    }
+}
